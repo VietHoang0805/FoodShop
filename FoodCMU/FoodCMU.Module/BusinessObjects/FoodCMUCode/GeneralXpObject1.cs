bed9adc265f0259ce2a7d682d7c38ca528fc0df0
VietHoang0805 <hoangviet852004@gmail.com>
2024-10-11 16:19:55 +0700

    Push setup first FoodShop

---
+++ FoodCMU/FoodCMU.Module/BusinessObjects/FoodCMUCode/GeneralXpObject1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using DevExpress.Xpo;
+using DevExpress.Xpo.Metadata;
+using DevExpress.Data.Filtering;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+namespace FoodCMU.Module.BusinessObjects.FoodCMU
+{
+
+    public partial class GeneralXpObject1
+    {
+        public GeneralXpObject1(Session session) : base(session) { }
+        public override void AfterConstruction() { base.AfterConstruction(); }
+    }
+
+}
